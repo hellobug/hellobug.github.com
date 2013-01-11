6c7d11015841b8637f4f282177ca23cb3b86a7ff
xiaochong <joy.xczhang@gmail.com>
2013-01-11 22:58:56 +0800

    Site updated at 2013-01-11 14:58:56 UTC

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,12 +1,9 @@
 var github = (function(){
-  function escapeHtml(str) {
-    return $('<div/>').text(str).html();
-  }
   function render(target, repos){
     var i = 0, fragment = '', t = $(target)[0];
 
     for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+escapeHtml(repos[i].description||'')+'</p></li>';
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
     }
     t.innerHTML = fragment;
   }
@@ -18,7 +15,7 @@ var github = (function(){
         , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
         , success: function(data) {
           var repos = [];
-          if (!data || !data.data) { return; }
+          if (!data.data) { return; }
           for (var i = 0; i < data.data.length; i++) {
             if (options.skip_forks && data.data[i].fork) { continue; }
             repos.push(data.data[i]);
